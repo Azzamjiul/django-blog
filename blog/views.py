2e04e469e904ad6534195091c36e84beaae663d5
azzam <azzam.jiul@gmail.com>
2020-07-18 23:02:41 +0700

    add comment count and view

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post })
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': post.comments.all })
 
 @login_required
 def post_new(request):
